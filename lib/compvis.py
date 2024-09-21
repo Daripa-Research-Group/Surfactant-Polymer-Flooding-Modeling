0f15d999841ff8c638aff3a87f2e609bc84c4e60
Bhargav <bhargav.akula1@gmail.com>
2024-09-21 19:18:30 -0400

    new branch to fix merge conflict

--- lib/compvis.py
+++ lib/compvis.py
@@ -5,82 +5,65 @@ import numpy as np
 # miuo = Displaced phase viscosity, c = polymer concentration
 #
 
-def compvis(c, U, V, X, Y):
-    global viscosityFlag
-    global miuw, miuo, beta1, c0, miup_array, c0_array, polymerType
-    
+### INITIALIZING GLOBAL PARAMETERS HERE!
+from .. import master_surf_grid 
+from . import Exceptions
+
+def compvis(c, U, V, X, Y, params, viscosity):
+    ### Initializing variables:
     gamma_dot = np.zeros_like(c)
-    n, m = c.shape
-    
-    if viscosityFlag == 1:
+    viscosityFlag = params['model_type']
+    vis_water = viscosity['water']
+    vis_oil = viscosity['oil']
+    vis_polymer = viscosity['polymer']
+    polymer_type = params['polymer_type']
+    beta1 = params['beta1']
+    c0 = params['c0']
+    c0_array = params['c0_array']
+
+    if ( viscosityFlag == 1 ):
+        # Newtonian Model (NO SHEAR THINNING INVOLVED => MODEL TYPE #1)
+        n = np.shape(( c,1 ))
+        m = np.shape(( c,2 ))
         if c0 == 0:
-            miua = miuw * np.ones((n, m))
+            vis_aqueous = vis_water * np.ones((n, m))
         else:
-            miua = miuw * (1 + beta1 * c)  #miuo*(0.5+c);#
-    elif viscosityFlag == 2:
-        # Sourav's implementation
-        if c0 == 0:
-            miua = miuw * np.ones(n)
+            vis_aqueous = vis_water * (1 + beta1 * c)
+    elif ( viscosityFlag == 2 ):
+        # Sourav's Implementation (MODEL TYPE #2)
+        n = np.shape((c,1))
+        if(c0 == 0):
+            vis_aqueous = vis_water*np.ones(n)
         else:
-            miua = miuo * (0.5 + c)
-    elif viscosityFlag == 3:
-        # Dynamic Viscosity Model (Non-Newtonian Model)
-        rho_water = 1000
-        rho_xanthane = 1500
+            vis_aqueous = vis_oil*(0.5+c)
+    elif( viscosityFlag == 3 ):
+        # Dynamic Viscosity (SHEAR THINNING ON => MODEL TYPE #3)
+        rho_water = 1000 # kg/m^3
+        rho_xanthane = 1500 #kg/m^3
+        rho_schizophyllan = 1300 #kg/m^3
         
-        w1 = rho_xanthane * c
-        w2 = rho_water * (1 - c)
-        wppm = (w1 / (w1 + w2)) * 1e6
+        if(polymer_type == 0):
+            #Xanthane polymer
+            w1 = rho_xanthane * c
+            w10 = rho_xanthane * c0_array
+        elif(polymer_type == 1):
+            #Schizophyllan polymer
+            w1 = rho_schizophyllan * c
+            w10 = rho_schizophyllan * c0_array
+        else:
+            print("<will raise exception here>")
+            exit(1)
+        
+        w2 = rho_water * (1-c)
+        w20 = rho_water * (1- c0_array)
 
-        w10 = rho_xanthane * c0_array
-        w20 = rho_water * (1 - c0_array)
-        wppm0 = (w10 / (w10 + w20)) * 1e6     
-#         [ 4.86265534 -0.41570227] (Schizo – n)
-#         [0.03647214 1.32175949] (Schizo – epsilon)
-#                 
-#         [ 3.05428284 -0.27294817] (Xanthane – n)
-#         [1.15410398e-04 2.04937780e+00] (Xanthane – epsilon)   
+        wppm0 = (w10 / (w10 + w20)) * 1*10^(6)
 
-    if polymerType:
-        epsCoeff = [0.03647214, 1.32175949]
-        nCoeff = [4.86265534, -0.41570227]
-    else:
-        epsCoeff = [1.15410398e-04, 2.04937780e+00]
-        nCoeff = [3.05428284, -0.27294817]
+        #determining the epsilon and n values for the power law equation:
 
-    epsilon0 = epsCoeff[0] * np.power(wppm0, epsCoeff[1])
-    power_n0 = np.minimum(nCoeff[0] * np.power(wppm0, nCoeff[1]), 1)
-    epsilon = epsCoeff[0] * np.power(wppm, epsCoeff[1])
-    power_n = np.minimum(nCoeff[0] * np.power(wppm, nCoeff[1]), 1)
+        
 
-#         epsilon0(:) = 250.*ones;
-#         power_n0(:) = 0.5.*ones;
-#         epsilon(:) = 250.*ones;
-#         power_n(:) = 0.5.*ones;
 
-    miua = miuw * np.ones((n, m))
-    a1 = np.sum(np.gradient(X, axis=(0, 1), axis=(1, 2)))
-    a2 = np.sum(np.gradient(Y, axis=(0, 1), axis=(1, 2)))
-    a3 = np.sum(np.gradient(X, axis=(1, 2), axis=(0, 1)))
-    a4 = np.sum(np.gradient(Y, axis=(1, 2), axis=(0, 1)))
-    pi_D = np.abs(-0.25 * ((a1 + a2) ** 2) + a3 * a4)
-            
-    for ii in range(n):
-        for jj in range(m):
-            if c[ii, jj] > 0:
-                gamma_dot[ii, jj] = 2 * np.sqrt(pi_D[ii, jj])
-                if gamma_dot[ii, jj] == 0:
-                    pass
-                else:
-                    miup_array[ii, jj] = epsilon0[ii, jj] * (gamma_dot[ii, jj] ** (power_n0[ii, jj] - 1))
-                    miua[ii, jj] = epsilon[ii, jj] * (gamma_dot[ii, jj] ** (power_n[ii, jj] - 1))
-                    if miua[ii, jj] < miuw:
-                        miua[ii, jj] = miuw
-                    if miua[ii, jj] > 100:
-                        miua[ii, jj] = 100
-                    if miup_array[ii, jj] < miuw:
-                        miup_array[ii, jj] = miuw
-                    if miup_array[ii, jj] > 100:
-                        miup_array[ii, jj] = 100
-                        
-    gammaMax = np.max(gamma_dot)
+
+    pass
+
---
+++ lib/para.py
@@ -0,0 +1,28 @@
+"""
+This class hold the para and box classes which will be used to formulate the mesh grid
+
+@author: Bhargav Akula Ramesh Kumar
+"""
+
+class Para:
+    def __init__(self):
+        '''Initializing default values for generating mesh grid for simulation'''
+        self.m = 100  # Number of points in x-direction
+        self.n = 100  # Number of points in y-direction
+        self.dx = None  # Spacing in x-direction (to be calculated later)
+        self.dy = None  # Spacing in y-direction (to be calculated later)
+
+class Box(Para):
+    def __init__(self):
+        super().__init__()  # Initialize the parent class (Para)
+        self.left = 0    # x_min
+        self.right = 1   # x_max
+        self.bottom = 0  # y_min
+        self.top = 1     # y_max
+
+    @property
+    def calculate_spacing(self):
+        # Calculate dx and dy based on the box dimensions
+        self.dx = (self.right - self.left) / (self.m - 1)
+        self.dy = (self.top - self.bottom) / (self.n - 1)
+
