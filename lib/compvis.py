6f8dc37b0056addac84aff0d01758e43760b7827
Bhargav <bhargav.akula1@gmail.com>
2024-09-24 21:11:48 -0400

    updating lib/compvis.py file

--- lib/compvis.py
+++ lib/compvis.py
@@ -5,7 +5,6 @@ import numpy as np
 # miuo = Displaced phase viscosity, c = polymer concentration
 #
 
-### INITIALIZING GLOBAL PARAMETERS HERE!
 from .. import master_surf_grid 
 from . import Exceptions
 
@@ -60,10 +59,49 @@ def compvis(c, U, V, X, Y, params, viscosity):
         wppm0 = (w10 / (w10 + w20)) * 1*10^(6)
 
         #determining the epsilon and n values for the power law equation:
+        epsilon_coefficients = {
+            "xanthane" : [1.15410398e-04, 2.04937780e+00],
+            'schizophyllan' : [0.03647214, 1.32175949]
+        }
+        n_coefficients = {
+            "xanthane" : [3.05428284, -0.27294817],
+            "schizophyllan" : [ 4.86265534, -0.41570227]
+        }
 
+        if(polymer_type == 0):
+            e_coeff = epsilon_coefficients['xanthane']
+            n_coeff = n_coefficients['xanthane']
+        else:
+            e_coeff = epsilon_coefficients['schizophyllan']
+            n_coeff = n_coefficients['schizophyllan']
+
+            
+        e_power_value = pow((e_coeff[0]*wppm0), e_coeff[1])
+        n_power_value = min(pow((n_coeff[0]*wppm0), n_coeff[1]))
+
+        n = np.shape((c,1))
+        m = np.shape((c,2))
+
+        vis_aqueous = vis_water * np.ones((n,m))
         
 
 
-
-    pass
-
+def divergence(X, V):
+    """
+    Compute the divergence of a vector field V with respect to spatial coordinates X.
+    
+    Parameters:
+    X : list of arrays
+        Spatial coordinates. For 2D: [x, y], for 3D: [x, y, z]
+    V : list of arrays
+        Vector field components. For 2D: [Vx, Vy], for 3D: [Vx, Vy, Vz]
+        
+    Returns:
+    div : array
+        Divergence of the vector field
+    """
+    div = np.zeros_like(V[0])
+    for i in range(len(X)):
+        grad = np.gradient(V[i], X[i])
+        div += grad[i]
+    return div
