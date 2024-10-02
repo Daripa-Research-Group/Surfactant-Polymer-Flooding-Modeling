6f2d0411ef2d5ad9d6c2e4ebead2aa8722aaec2b
Bhargav <bhargav.akula1@gmail.com>
2024-10-02 19:49:46 -0400

    updating repo __init__ functions for the surfactant, polymer, and simulation classes

--- lib/simulation.py
+++ lib/simulation.py
@@ -5,9 +5,10 @@ This python script contains the class definition for running simulations
 from .surfactant import Surfactant
 from .polymer import Polymer
 from . import Exceptions
+from .para import Box
 
 class Simulation:
-    def __init__(self, sim_id, polymer, surfactant, resevoir_geometry, permeability_flg):
+    def __init__(self, sim_id, polymer, surfactant, resevoir_geometry, permeability_flg, mesh_grid):
         """
         creates instance of the simulation class which will enable for calculating changes in system parameters at every time-step
 
@@ -25,6 +26,9 @@ class Simulation:
 
         :param permeability_flg: Homogenous vs. Heterogenous porosity in resevoir
         :type permeability_flg: str
+
+        :param mesh_grid: mesh_grid used in the SP-flooding run
+        :type mesh_grid: Box
         """
         
         # PROTECTED MEMBERS
@@ -32,6 +36,7 @@ class Simulation:
         self._surfactant_ = None
         self._resevoir_geometry_ = None
         self._permeability_flag_ = None
+        self._mesh_ = None
         
         #PUBLIC MEMBERS
         self.sim_id = sim_id
@@ -39,6 +44,7 @@ class Simulation:
         self.surfactant = surfactant
         self.resevoir_geometry = resevoir_geometry
         self.permeability_flg = permeability_flg
+        self.mesh = mesh_grid
 
     @property
     def polymer(self):
@@ -71,4 +77,12 @@ class Simulation:
     @permeability_flg.setter
     def permeability_flg(self, value):
         self._permeability_flag_ = value
+
+    @property
+    def mesh(self):
+        return self._mesh_
+
+    @mesh.setter
+    def mesh(self, value):
+        self._mesh_ = value
 
