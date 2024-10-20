70770f4c1dd6f387f5621d301f3073f60599d504
carlosra0345 <carlosra0345@gmail.com>
2024-10-21 01:07:14 +0200

    add json input handler

---
+++ utils/input_handler.py
@@ -0,0 +1,15 @@
+from typing import List
+import json
+
+################################################################
+# This utility handles user inputs and settings configuration. #
+################################################################
+        
+def get_user_input(file_name : str) -> List[dict]:
+    simulations : List[dict] = []
+    with open(file_name, 'r') as input_file:
+                input_simulations = json.load(input_file)
+                for simulation in input_simulations:
+
+                    simulations.append(simulation)
+    return simulations
