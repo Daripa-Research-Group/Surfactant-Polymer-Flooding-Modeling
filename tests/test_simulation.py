325ca191280fa8ce29bea172e992e43cd02afc29
Bhargav <bhargav.akula1@gmail.com>
2024-12-11 19:04:26 -0500

    adding test functions

--- tests/test_simulation.py
+++ tests/test_simulation.py
@@ -1,3 +1,20 @@
 """
 This file should hold the test cases for the 'simulation.py' class
 """
+
+
+def test_initializing_simulation():
+    pass
+
+def test_get_phi_value():
+    pass
+
+def test_z_func_calculation():
+    pass
+
+def test_initialize_concentration():
+    pass
+
+def test_compute_viscosity():
+    pass
+
