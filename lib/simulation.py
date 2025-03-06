224b0b4c3c4bd41b686224e147e94ac5418558f2
Bhargav <bhargav.akula1@gmail.com>
2025-03-06 07:52:43 -0500

    updating saturation_equ_solver() method

--- lib/simulation.py
+++ lib/simulation.py
@@ -1016,7 +1016,17 @@ class Simulation:
                         if(i == 0):
                             DD[i] = (Qmod[cnt][i]/dt[cnt][i]) \
                                     + ((D_g[cnt][i] + D_g[cnt][i])/(dx**2) + (D_g[cnt-1][i]+D_g[cnt][i])/(dy**2))*self.surfactant.vec_concentration[cnt][i] \
-                                    # + 
+                                    - (D_g[cnt][i]+D_g[cnt][i+1])/(dx**2)*self.surfactant.vec_concentration[cnt][i+1]\
+                                    - (D_g[cnt][i] + D_g[cnt-1][i])/(dy**2)*self.surfactant.vec_concentration[cnt-1][i]
+
+                            AA[j][i] = (D_s[cnt][i]+D_s[cnt-1][i])/(dy**2)
+                            
+                            BB[j][i] = 1/dt[cnt][i]-(D_s[cnt][i]+D_s[cnt][i+1])/(dx**2)\
+                                    -(D_s[cnt-1][i]+D_s[cnt][i])/(dy**2)
+                            
+                            BB[j][i+1] = (D_s[cnt][i]+D_s[cnt][i+1])/(dx**2)
+                        elif(i == m):
+                            pass
 
 
 
