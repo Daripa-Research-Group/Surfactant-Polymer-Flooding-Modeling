03fe5581c81249854520118f807c1ec0d3fc63bb
Bhargav Akula <mail.bhargavakula@gmail.com>
2025-05-07 08:04:26 -0400

    updated the polymer class structure

--- lib/polymer.py
+++ lib/polymer.py
@@ -4,7 +4,7 @@ This python script contains the class definition for polymers for the surfactant
 The methods of this class were derived from the MATLAB Surfactant-Polymer Flooding Code developed by
 Sourav Dutta and Rohit Mishra.
 
-@author: Bhargav Akula Ramesh Kumar
+@author: Bhargav Akula Ramesh Kumar, Carlos Acosta Caripo
 """
 
 import numpy as np
@@ -19,6 +19,7 @@ class Polymer:
         viscosity_scalar=None,
         viscosity_matrix=None,
         vec_concentration=None,
+        shear_rate = None
     ):
         """
         Initializes a instance of the polymer class
@@ -26,8 +27,8 @@ class Polymer:
         :param name: Name of the polymer
         :type name: enum 'PolymerList'
 
-        :param initial_concentration: Initial concentration of polymer in the injected solution (scalar variable)
-        :type concentration: float
+        :param concentration_scalar: holds the scalar quantity of the polymer concentration within the resevoir 
+        :type concentration_scalar: float
 
         :param e_coeff: The coefficients used to determine epsilon for the empirical power law expression used to determine the viscosity of the aqueous phase
         :type e_coeff: List<int>
@@ -39,21 +40,44 @@ class Polymer:
         :type viscosity_matrix: float, None
 
         :param viscosity_matrix: viscosity matrix of the polymer
-        :type viscosity_matrix: np.array, None
+        :type viscosity_matrix: np.ndarray, None
 
-        :param vec_concentration: vector representation of polymer concentration within resevoir
-        :type vec_concentration: np.array, None
+        :param concentration_matrix: matrix representation of polymer concentration within resevoir
+        :type concentration_matrix: np.ndarray, None
+
+        :param shear_rate: Matrix that will hold the shear rate (the change in velocity normal to the direction of flow)
+        :type shear_rate: np.ndarray, None
         """
-
+        
+        #PolymerList object
         self.name = name
-        self.initial_concentration = initial_concentration
-        self.vec_concentration = vec_concentration
+
+        #properties related to the concentration
+        self.concetration_scalar = initial_concentration
+        self.concentration_matrix = vec_concentration
+        
+        #Properties related to the viscosity
         self.viscosity_matrix = viscosity_matrix
         self.viscosity_scalar = viscosity_scalar
+
+        #Values required to formulate the numerical powerlaw function for viscosity calculations
         self.e_coeff = e_coeff
         self.n_coeff = n_coeff
 
-    def compute_viscosity(self, grid):
+        #shear rate matrix (needed when running 'shear thinning' model version)
+        self.shear_rate = shear_rate
+
+    def initialize(self):
+        """
+        Will initialize the viscosity and concentration matrices
+
+        :return: a list of the initialized concentration and viscosity matrices
+        :rtype: List
+        """
+        pass
+
+
+    def compute_viscosity(self, grid, model_type):
         """
         Compute polymer viscosity.
         This function is derived from 'compvis()' in the original MATLAB code.
@@ -61,15 +85,18 @@ class Polymer:
         :param grid: The FEM grid used for simulation calculations
         :type grid: np.ndarray
 
+        :param model_type: Will state whether the model will include polymer shear thinning or not
+        :type model_type: enum 'ModelType'
+
         :return: the viscosity_matrix for the polymer within the grid
         :rtype: np.ndarrray
         """
-        
         pass
+
 
     def compute_concentration(self, grid, u, v):
         """
-        Update the polymer concentration matrix property
+        Update the polymer concentration matrix and the shear rate tensor
 
         This function is derived from the section of the 'nmmoc_surf_mod_neumann'
         related to the polymer concentration matrix
