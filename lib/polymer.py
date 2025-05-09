fc98e79b49b029bb771eafa73249e9ca3f45f6e7
Bhargav Akula <mail.bhargavakula@gmail.com>
2025-05-09 08:35:03 -0400

    implementing power law for computing_viscosity function

--- lib/polymer.py
+++ lib/polymer.py
@@ -8,7 +8,8 @@ Sourav Dutta and Rohit Mishra.
 """
 
 import numpy as np
-from enumerations import ModelType, SimulationConstants
+from enumerations import ModelType, PolymerList, SimulationConstants
+from lib.Exceptions import SimulationCalcInputException
 
 class Polymer:
     """
@@ -20,10 +21,11 @@ class Polymer:
         initial_concentration,
         e_coeff,
         n_coeff,
+        rho,
         viscosity_scalar=None,
         viscosity_matrix=None,
         vec_concentration=None,
-        shear_rate = None
+        shear_rate=None
     ):
         """
         Initializes a instance of the polymer class
@@ -39,6 +41,9 @@ class Polymer:
 
         :param n_coeff:  The coefficients used to determine epsilon for the empirical power law expression used to determine the viscosity of the aqueous phase
         :type n_coeff: List<int>
+
+        :param rho: Density of polymer
+        :type rho: float
 
         :param viscosity_scalar: scalar quantitiey of the polymer viscosity
         :type viscosity_matrix: float, None
@@ -56,8 +61,9 @@ class Polymer:
         #PolymerList object
         self.name = name
 
-        #properties related to the concentration
+        #properties related to the concentration (scalar concentration, matrix version of initial concentration, and current concentration matrix)
         self.concetration_scalar = initial_concentration
+        self.init_concentration_matrix = initial_concentration * np.ones((SimulationConstants.Grid_Size.value, SimulationConstants.Grid_Size.value))
         self.concentration_matrix = vec_concentration
         
         #Properties related to the viscosity
@@ -67,6 +73,9 @@ class Polymer:
         #Values required to formulate the numerical powerlaw function for viscosity calculations
         self.e_coeff = e_coeff
         self.n_coeff = n_coeff
+        
+        #Polymer Density
+        self.rho = rho 
 
         #shear rate matrix (needed when running 'shear thinning' model version)
         self.shear_rate = shear_rate
@@ -84,7 +93,7 @@ class Polymer:
     def compute_viscosity(self, grid, u, v, aqueous_viscosity, model_type):
         """
         Compute polymer viscosity.
-        This function is derived from 'compvis()' in the original MATLAB code.
+        This function is derived from 'compvis()' in the original MATLAB code (in file compvis.m).
 
         :param grid: The FEM grid used for simulation calculations (x and y variables from the MATLAB code)
         :type grid: np.ndarray
@@ -108,6 +117,9 @@ class Polymer:
         x = grid[0]
         y = grid[1]
 
+        if(self.concentration_matrix is None or self.init_concentration_matrix is None or self.concetration_scalar is None):
+            raise SimulationCalcInputException("SimulationInputException: Polymer concentration matrix and/or scalar concentration value not initialized...") 
+
         #if model_type is NO SHEAR THINNING:
         if(model_type == ModelType.No_Shear_Thinning.value):
             ## the scalar viscosity is equal to the max within the aqueous viscosity matrix
@@ -119,9 +131,24 @@ class Polymer:
             pass
         #if polymer shear thinning is ON:
         elif(model_type == ModelType.Shear_Thinning_On.value):
-            # Will need to use the numerically derived power law to determine the viscosity of the polymer
+            # Getting water density (Note: polymer density a property of class)
+            rho_water = SimulationConstants.Water_Density.value
 
-            pass
+            # Formulating the numerically derived power law equation
+            w1 = self.rho*self.concentration_matrix
+            w2 = rho_water*(1-self.concentration_matrix)
+            wppm = (w1/(w1+w2))*(10**6)
+            w1_0 = self.rho*self.init_concentration_matrix #from the variable w10 in MATLAB code
+            w2_0 = rho_water*(1-self.init_concentration_matrix) #from the variable w20 in MATLAB code
+            wppm_0 = (w1_0/(w1_0+w2_0))*(10**6) #from the wppm0 variable in MATLAB code
+
+            epsilon_0 = self.e_coeff[0]*(wppm_0**self.e_coeff[1])
+            n_0 = np.min(self.n_coeff[0]*(wppm_0**self.n_coeff[1]))
+            epsilon = self.e_coeff[0]*(wppm**self.e_coeff[1])
+            n = np.min(self.n_coeff[0]*(wppm**self.n_coeff[1]))
+
+            
+
 
     def compute_concentration(self, grid, u, v):
         """
