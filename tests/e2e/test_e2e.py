1b4d025a88ee5c738a5cd682b161354ed651acff
carlosra0345 <carlosra0345@gmail.com>
2026-03-08 11:31:05 -0500

    create e2e tests

--- tests/e2e/test_e2e.py
+++ tests/e2e/test_e2e.py
@@ -2,31 +2,22 @@ import sys
 import os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../lib")))
+
 import pytest
 import numpy as np
 from numpy.testing import assert_allclose
 from lib.simulation import Simulation
-from lib.polymer import Polymer
-from lib.surfactant import Surfactant
-from lib.enumerations import (
-    PolymerList,
-    ModelType,
-    PermeabilityType,
-    PlotType,
-    SurfactantList,
-    ResevoirGeometry,
-    SimulationConstants,
-)
 
 MODEL = {
-    "No Shear Thinning": 1,
-    "Sourav Implementation": 2,
-    "Shear Thinning": 3,
+    "No_Shear_Thinning": 1,
+    "Sourav_Implementation": 2,
+    "Shear_Thinning": 3,
 }
 
 GEOMETRY = {
     "Rectilinear": 1,
-    "Quarter Five Spot": 2,
+    "Quarter_Five_Spot": 2,
 }
 
 PERMEABILITY = {
@@ -37,46 +28,48 @@ PERMEABILITY = {
 POLYMER = {
     "Xanthane": 1,
     "Schizophyllan": 2,
-    "No Polymer": 3,
+    "No_Polymer": 3,
 }
 
 SURFACTANT = {
-    "Alkyl Ether Sulfate": 1,
-    "No Surfactant": 2,
+    "Alkyl_Ether_Sulfate": 1,
+    "No_Surfactant": 2,
 }
+
+TRUE_VALUES_DIR = os.path.join(os.path.dirname(__file__), "true_values")
 
 
 def load_true_values(sim_id):
-    base = f"true_values/sim_{sim_id}_"
     return {
-        "COC": np.load(base + "COC.npy"),
-        "MFW": np.load(base + "MFW.npy"),
+        "COC": np.load(os.path.join(TRUE_VALUES_DIR, f"sim_{sim_id}_COC.npy")),
+        "MFW": np.load(os.path.join(TRUE_VALUES_DIR, f"sim_{sim_id}_MFW.npy")),
     }
 
 
+@pytest.mark.e2e
 @pytest.mark.parametrize(
     "simulation_id, model_type, reservoir_geometry, permeability, polymer_type, polymer_concentration, surfactant_type, surfactant_concentration",
     [
         (
             1,
-            MODEL["Shear Thinning"],
+            MODEL["Shear_Thinning"],
             GEOMETRY["Rectilinear"],
             PERMEABILITY["Homogeneous"],
             POLYMER["Xanthane"],
             0.001,
-            SURFACTANT["No Surfactant"],
+            SURFACTANT["No_Surfactant"],
             0,
-        )  # ,
-        # (
-        #     2,
-        #     MODEL["No Shear Thinning"],
-        #     GEOMETRY["Rectilinear"],
-        #     PERMEABILITY["Homogeneous"],
-        #     POLYMER["Schizophyllan"],
-        #     0.001,
-        #     SURFACTANT["No Surfactant"],
-        #     0,
-        # ),
+        ),
+        (
+            2,
+            MODEL["No_Shear_Thinning"],
+            GEOMETRY["Rectilinear"],
+            PERMEABILITY["Homogeneous"],
+            POLYMER["Schizophyllan"],
+            0.001,
+            SURFACTANT["No_Surfactant"],
+            0,
+        ),
     ],
 )
 def test_e2e(
@@ -89,53 +82,34 @@ def test_e2e(
     surfactant_type,
     surfactant_concentration,
 ):
-    plot_type = PlotType.Saturation_Plot  # TODO: MAKE DYNAMIC
+    user_dict = {
+        "simulation_id": simulation_id,
+        "model_type": model_type,
+        "reservoir_geometry": reservoir_geometry,
+        "permeability": permeability,
+        "polymer_type": polymer_type,
+        "polymer_concentration": polymer_concentration,
+        "surfactant_type": surfactant_type,
+        "surfactant_concentration": surfactant_concentration,
+    }
 
-    model_type = ModelType(model_type)
-    reservoir_geometry = ResevoirGeometry(reservoir_geometry)
-    permeability_flag = PermeabilityType(permeability)
-    polymer_type = PolymerList.get_by_value(polymer_type)
-    polymer_concentration = polymer_concentration
-    surfactant_type = SurfactantList(surfactant_type)
-    surfactant_concentration = surfactant_concentration
+    sim = Simulation(user_input_dict=user_dict)
+    sim.run()
 
-    polymer_obj = Polymer(
-        name=polymer_type,
-        initial_concentration=polymer_concentration,
-        e_coeff=polymer_type.e_coeff,
-        n_coeff=polymer_type.n_coeff,
-    )
+    expected = load_true_values(simulation_id)
 
-    surfactant_obj = Surfactant(
-        name=surfactant_type,
-        initial_concentration=surfactant_concentration,
-        IFT_conc_equ=lambda GG: 10.001 / (GG + 1)
-        - 0.001,  # TODO: make dynamic depending on the surfactant type
-        derivative_IFT_conc_equ=lambda GG: (-10.001)
-        / ((GG + 1) ** 2),  # TODO: make dynamic depending on the surfactant type
-    )
-
-    SOG = SimulationConstants.Grid_Size.value
-
-    simulation = Simulation(
-        sim_id=simulation_id,
-        size_of_grid=SOG,
-        polymer=polymer_obj,
-        surfactant=surfactant_obj,
-        resevoir_geometry=reservoir_geometry,
-        permeability_flg=permeability_flag,
-        mdl_id=model_type,
-        plt_type=plot_type,
-    )
-
-    simulation_output = simulation.execute_simulation()
-    expected_output = load_true_values(simulation_id)
-
-    # compare simulation output with expected output
     assert_allclose(
-        simulation_output["COC"],
-        expected_output["COC"],
+        sim.COC,
+        expected["COC"],
         rtol=1e-5,
         atol=1e-8,
-        err_msg=f"COC mismatch in e2e test for simulation id = {simulation_id}",
+        err_msg=f"COC mismatch for simulation_id={simulation_id}",
+    )
+
+    assert_allclose(
+        np.array(sim.MFW),
+        expected["MFW"],
+        rtol=1e-5,
+        atol=1e-8,
+        err_msg=f"MFW mismatch for simulation_id={simulation_id}",
     )
