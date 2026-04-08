3948fd15267fd7e06049d551795763d4e0744157
Bhargav Akula <mail.bhargavakula@gmail.com>
2026-04-08 17:19:54 -0400

    adding solver.py
    this module will hold the methods and parameters relevant to solving the
    transport equation and the elliptic equations for this simulation.

---
+++ lib/solver.py
@@ -0,0 +1,22 @@
+"""
+Will hold the classes relevant for solving the elliptic equations and the transport equations
+
+The methods of this class were derived from the MATLAB Surfactant-Polymer Flooding Code developed by
+Sourav Dutta and Rohit Mishra.
+
+@author: Bhargav Akula Ramesh Kumar, Carlos Acosta Caripo
+"""
+
+import simulation
+
+class TransportEquationSolver(simulation.Simulation):
+    """
+    Will hold all the relevant functions and parameters necessary for solving the transport equations
+    """
+    pass
+
+class EllipticEquationSolver(simulation.Simulation):
+    """
+    Will hold the relevant functions and parameters for solving the global pressure (u) and velocity (v)
+    """
+    pass
