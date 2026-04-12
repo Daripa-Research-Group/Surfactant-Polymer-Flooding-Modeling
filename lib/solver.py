9f6efed6d155e6efbaf6b94a81c9c9521cc5436a
Bhargav Akula <mail.bhargavakula@gmail.com>
2026-04-12 12:28:26 -0400

    adding basic framework for ```TransportEquationSolver``` class

--- lib/solver.py
+++ lib/solver.py
@@ -19,16 +19,34 @@ class TransportEquationSolver():
             water_saturation,
             surfactant_concentration,
             polymer_concentration,
-
+            aqueous_mobility,
+            aqueous_viscosity,
+            oleic_mobility,
+            oleic_viscosity,
             
     ):
         """
         constructor for ``TransportEquationSolver``
         """
+        pass
+
+    def execute(self):
+        pass
+
+    def saturation_matrix_preprocessing(self):
+        pass
+
+    def surfactant_concentration_matrix_preprocessing(self):
+        pass
+
+    def polymer_concentration_matrix_preprocessing(self):
+        pass
         
 
-class EllipticEquationSolver():
-    """
-    Will hold the relevant functions and parameters for solving the global pressure (u) and velocity (v)
-    """
-    pass
+# class EllipticEquationSolver():
+#     """
+#     Will hold the relevant functions and parameters for solving the global pressure (u) and velocity (v)
+#
+#     (Not implemented for v1)
+#     """
+#     pass
